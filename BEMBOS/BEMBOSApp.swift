793590ae25e48db31979864e9ed4d79b7013f5f2
adriansotoh <adriansotohidalgo16@gmail.com>
2022-10-22 19:34:15 -0500

    Initial Commit

---
+++ BEMBOS/BEMBOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  BEMBOSApp.swift
+//  BEMBOS
+//
+//  Created by qury on 22/10/22.
+//
+
+import SwiftUI
+
+@main
+struct BEMBOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
